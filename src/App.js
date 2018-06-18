bb0961793903d5a9f234d4183c97c3907cad89a8
Ro Diwa <rodiwaa@gmail.com>
2018-06-19 00:06:01 +0530

    removed redundant header code

--- src/App.js
+++ src/App.js
@@ -3,19 +3,10 @@ import './style/App.css';
 
 import Body from './components/Body'
 
-const Header = () => <header className="header"><h1>Word Cloud</h1></header>
-
 class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      showHeader: false
-    }
-  }
   render() {
     return (
       <div className="App">
-        {this.state.showHeader && <Header />}
         <Body />
       </div>
     );
