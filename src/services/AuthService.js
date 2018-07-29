33a670944bd3caa173cd3185b3bb13d0d9be4cbf
Ro Diwa <rodiwaa@gmail.com>
2018-07-29 10:30:31 +0530

    wip for firebase auth

---
+++ src/services/AuthService.js
@@ -0,0 +1,38 @@
+import firebase from 'firebase'
+import firebaseConfig from './FirebaseService'
+
+export const signIn = () => {
+  console.log('signIn')
+  const provider = new firebase.auth.GoogleAuthProvider()
+  return firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(result => {
+      // const { uid, displayName } = result
+      console.log(result)
+      console.log('Logged in now')
+      console.log(firebase.auth().currentUser)
+      // firebase.database().ref(`user/${uid}`).set({
+      //   displayName 
+      // })
+    })
+    .catch(err => {
+      console.log('No logged in', err)
+      console.error(err)
+    })
+}
+
+export const signOut = () => {
+  firebase.auth().signOut()
+    .then(() => {
+      console.log('signed out')
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+}
+
+export const isLoggedIn = () => {
+  console.log(firebase.auth().currentUser)
+  return firebase.auth().currentUser
+}
---
+++ src/services/FirebaseService.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyBL3w2IhCmrzfR3Ze0_6c1lVOxFBD368gk",
+  authDomain: "wordee-e6ac5.firebaseapp.com",
+  databaseURL: "https://wordee-e6ac5.firebaseio.com",
+  projectId: "wordee-e6ac5",
+  storageBucket: "wordee-e6ac5.appspot.com",
+  messagingSenderId: "5871169658"
+}
+
+export default firebase.initializeApp(config)
